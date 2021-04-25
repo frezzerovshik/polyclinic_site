82dfb02ec4b3e728d11996339f3a06f9b378d4e8
Vadim Ryauka <ryauka.vadim@e-legion.com>
2021-04-25 16:05:46 +0300

    Implement patient registration

---
+++ patient_administrating.php
@@ -0,0 +1,24 @@
+<?php
+$patient = $_POST['patient'];
+if (strlen($patient)==0)
+{
+    die("Ошибка при передаче данных");
+}
+switch ($patient) {
+    case 'reg':
+        include_once("patient_reg.html");
+        break;
+    case 'patientslist':
+        include_once("patient_list.php");
+        break;
+    case 'todoc':
+        include_once("patient_todoc.php");
+        break;
+    case 'covid':
+        include_once("patient_covid.php");
+        break;
+    default:
+        echo "Неизвестная ошибка</br>";
+        break;
+}
+?>
