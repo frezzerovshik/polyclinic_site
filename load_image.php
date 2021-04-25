ddde245153b17ef798c91e42ab4df5a801e743ca
Vadim Ryauka <ryauka.vadim@e-legion.com>
2021-04-25 13:46:31 +0300

    Hot fix: remove unneeded connection to db

--- load_image.php
+++ load_image.php
@@ -1,7 +1,5 @@
 <?php
     $id = $_SESSION['id'];
-    require_once "connect_db.php";
-    $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Ошибка " . mysqli_error($conn));
     $id = $id + 1;
     $sql = "SELECT `image_url` FROM `Пользователь` WHERE `ID пользователя`='" . $id . "';";
     $result = $conn->query($sql);
